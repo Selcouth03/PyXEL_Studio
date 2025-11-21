6510c876bbf4ea6423663cc3ce9f902984aafc85
Abraham Selcouth <abraham.programacion10@gmail.com>
2025-11-20 20:18:32 -0400

    feat: Agregar lógica de colores y mejorar la interfaz de usuario

---
+++ src/logic.py
@@ -0,0 +1,32 @@
+PALETA_COLORES = [
+    # --- Escala de Grises ---
+    "#FFFFFF",  # Blanco
+    "#C0C0C0",  # Plata
+    "#808080",  # Gris
+    "#000000",  # Negro
+    # --- Rojos, Rosas y Piel ---
+    "#FFC0CB",  # Rosa
+    "#FF0000",  # Rojo
+    "#A52A2A",  # Marrón
+    "#800000",  # Marrón (Maroon)
+    # --- Naranjas y Amarillos ---
+    "#F5DEB3",  # Trigo (Tono piel claro)
+    "#D2691E",  # Chocolate (Tono piel oscuro)
+    "#FFA500",  # Naranja
+    "#FFD700",  # Dorado
+    "#FFFF00",  # Amarillo
+    # --- Verdes ---
+    "#808000",  # Oliva
+    "#00FF00",  # Lima
+    "#008000",  # Verde Oscuro
+    # --- Azules y Cianes ---
+    "#ADD8E6",  # Azul Claro
+    "#00FFFF",  # Cyan
+    "#008080",  # Verde Azulado (Teal)
+    "#0000FF",  # Azul
+    "#000080",  # Azul Marino
+    # --- Púrpuras y Magentas ---
+    "#FF00FF",  # Magenta
+    "#800080",  # Púrpura
+    "#4B0082",  # Índigo (Púrpura oscuro)
+]
